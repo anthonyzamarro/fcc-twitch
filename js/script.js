5ccdc3c694677525be876c63d380af420b8fecdd
Anthony Z <anthony.zamarro@gmail.com>
2017-05-08 16:09:36 -0400

    Add js, css, and html files

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+// https://api.twitch.tv/kraken/streams/71852806?client_id=uunztvf1km2sufh91mnk44yhaw6aww&api_version=5
+$(document).ready(function() {
+var channels = ["OgamingSC2", "ESL_SC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+var urlChannels = 'https://wind-bow.glitch.me/twitch-api/channels/';
+var urlStreams = 'https://api.twitch.tv/kraken/streams/';
+var newChannels, displayName, id, game, logo, followers, views, url, closed, none, stream_type, all, online, offline, live, liveNow;
+channels.forEach(function(c){
+  var list = document.createElement('li');
+  var span = document.createElement('span');
+  var players = document.getElementById('players');
+  var display = players.appendChild(list);
+      newChannels = urlChannels + c;
+console.log(none);
+  $.getJSON(newChannels, function(data) {
+    displayName = data.display_name;
+    id = data._id;
+    game = data.game;
+    logo = data.logo;
+    followers = data.followers;
+    views = data.views;
+    url = data.url;
+    closed = data.error;
+    $(display).append('<img src=' + logo + '>');
+    $(display).wrap('<a target="_blank" href=' + url + '></a>')
+    $(display).append(displayName);
+    $(display).append(span);
+    $(display).css('color', 'rgb(255, 236, 0)');
+    $('li').last().append(closed);
+    $('li').last().css('color', 'red');
+    idStreams = urlStreams + id + '?client_id=uunztvf1km2sufh91mnk44yhaw6aww&api_version=5';
+    $.getJSON(idStreams, function(data){
+            live = data.stream;
+            liveNow = data.stream.stream_type;
+            if(liveNow === 'live') {
+              $(span).css({'background': 'darkgreen'});
+              $(display).append( '<br>' + 'Game: '+ game + ' ' + 'followers: ' + followers + ' ' + ' ' +  'views: ' + views);
+              $(list).addClass('offline');
+            }
+
+        });
+
+    });
+
+
+       //all
+            $('#all').click(function(){
+              $(list).show()
+            });
+    //online
+            $('#online').click(function(){
+              if(!$(list).hasClass('online') ){
+                $(list).show();
+                if(!$(list).hasClass('offline')){
+                  $(list).hide();
+                }
+              }
+            });
+    //offline
+            $('#offline').click(function(){
+              $(list).show()
+              if($(list).hasClass('offline')){
+                $(list).show();
+                if(!$(list).hasClass('online') ){
+                  $(list).hide();
+                }
+              }
+            });
+  });
+
+});
